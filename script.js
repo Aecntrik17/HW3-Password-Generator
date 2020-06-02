754ea948f13c5503f24fd4087fe9396db0907804
Aaron Sims <aaron@adsimsllc.com>
2020-06-02 12:13:34 -0400

    creating password selection criteria variables

---
+++ script.js
@@ -0,0 +1,21 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Necessary variables for password selection criteria
+var number = "0123456789";
+var lowerLetter = "abcdefghijklmnopqrstuvwxyz";
+var capLetter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var special = "!@$%&#";
+var userChoice = ""
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
